78510acdf97fff79b920755363e1c840c80d1f37
Roberdan Andrade <Roberdan.andrade.dev@gmail.com>
2024-07-23 12:14:20 -0300

    Finalizando CRUD Postagens

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
 
@@ -11,4 +11,34 @@ export class PostagemController{
     FindAll(): Promise<Postagem[]> {
         return this.postagemServices.findAll();
     }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    FindById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+        return this.postagemServices.findById(id);
+    }
+
+    @Get('/titulo/:titulo')
+    @HttpCode(HttpStatus.OK)
+    FindByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+        return this.postagemServices.findByTitulo(titulo);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    Create(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemServices.create(postagem);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    Update(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemServices.update(postagem);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    Delete(@Param('id', ParseIntPipe) id: number){
+        return this.postagemServices.delete(id);
+    }    
 }
