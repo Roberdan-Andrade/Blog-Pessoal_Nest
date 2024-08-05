eae94f87a43d4a9ae879a63085740741e160b421
Roberdan Andrade <Roberdan.andrade.dev@gmail.com>
2024-08-05 16:12:14 -0300

    Ativando validaçoes no test

--- test/usuario.e2e-spec.ts
+++ test/usuario.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppModule } from '../src/app.module';
@@ -23,6 +23,7 @@ describe('Testes do modulos Usuarios e Auth (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
